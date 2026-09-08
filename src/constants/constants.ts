27175e9a3ddc430ee9ad0d528b4847687457dfa0
feroz <ferozali690@gmail.com>
2026-09-08 23:14:17 +0530

    refactor(constants): move API configuration to environment variables

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,3 @@
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-export const TMDB_API_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWYyMDY0NDEyZWI5ZjZhZWFkMmNiNGYxMjdmNWIxNiIsIm5iZiI6MTc2NTAxNjE2Ni4wMzIsInN1YiI6IjY5MzQwMjY2MGE2NjFkNjNkNmI3MGRiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WJIvF2pI45Pr_KeVmP30y5BJsKafjJrOY1JW2jMjXGc";
+export const TMDB_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+export const TMDB_IMAGE_BASE_URL = import.meta.env.VITE_TMDB_BASE_IMAGE_URL;
+export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
